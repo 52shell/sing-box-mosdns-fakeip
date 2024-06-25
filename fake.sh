e10017c174ccd58190feffc28fd49362500450a0
Tasia Donnelly <1733713379@qq.com>
2024-06-25 22:08:56 +0800

    更新 fake.sh

--- fake.sh
+++ fake.sh
@@ -1,3 +1,15 @@
-
-# check root
+#!/bin/bash
+# check root执行
 [[ $EUID -ne 0 ]] && echo -e "${red}错误：${plain} 必须使用root用户运行此脚本！\n" && exit 1
+
+check_status() {
+    if [[ ! -f /etc/systemd/system/sing-box.service ]]; then
+        return 2
+    fi
+    temp=$(systemctl status sing-box | grep Active | awk '{print $3}' | cut -d "(" -f2 | cut -d ")" -f1)
+    if [[ x"${temp}" == x"running" ]]; then
+        return 0
+    else
+        return 1
+    fi
+}
