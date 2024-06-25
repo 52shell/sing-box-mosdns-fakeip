870908344480c02bbb70b61a62afce9f9a178e54
Tasia Donnelly <1733713379@qq.com>
2024-06-25 22:14:19 +0800

    更新 fake.sh

--- fake.sh
+++ fake.sh
@@ -2,7 +2,7 @@
 # check root执行
 [[ $EUID -ne 0 ]] && echo -e "${red}错误：${plain} 必须使用root用户运行此脚本！\n" && exit 1
 
-check_status() {
+check_sing-box_status() {
     if [[ ! -f /etc/systemd/system/sing-box.service ]]; then
         return 2
     fi
@@ -13,3 +13,14 @@ check_status() {
         return 1
     fi
 }
+
+check_mosdns_status() {
+    if [[ ! -f /etc/systemd/system/mosdns.service ]]; then
+        return 2
+    fi
+    temp=$(systemctl status mosdns | grep Active | awk '{print $3}' | cut -d "(" -f2 | cut -d ")" -f1)
+    if [[ x"${temp}" == x"running" ]]; then
+        return 0
+    else
+        return 1
+    fi
