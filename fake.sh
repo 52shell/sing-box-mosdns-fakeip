c9862aa8b77c511a3d4435c0fc4135bf7fe0aa70
Tasia Donnelly <1733713379@qq.com>
2024-06-27 17:14:14 +0800

    更新 fake.sh

--- fake.sh
+++ fake.sh
@@ -1,93 +1,336 @@
 #!/bin/bash
-# check root执行
-[[ $EUID -ne 0 ]] && echo -e "${red}错误：${plain} 必须使用root用户运行此脚本！\n" && exit 1
+# 检查是否为root用户执行
+[[ $EUID -ne 0 ]] && echo -e "错误：必须使用root用户运行此脚本！\n" && exit 1
+
+# 定义函数提示用户输入
+prompt_user() {
+    echo -e "
+   Herozmy 自用脚本
+   请选择需要管理的程序
+————————————————
+  1. sing-box
+  2. mosdns
+  0. 退出脚本
+ "
+}
+
+
+
+checkcore() {
+    if [[ $system == "sing-box" ]]; then
+        show_menu_singbox
+    elif [[ $system == "mosdns" ]]; then
+        show_menu_mosdns
+    fi
+}
+
 show_status() {
-    check_sing-box_status
+    check_status
     case $? in
     0)
-        echo -e "sing-box: 已运行"
-        show_enable_status
+        echo -e "$system: 已运行"
+        
         ;;
     1)
-        echo -e "sing-box: 未运行"
-        show_enable_status
+        echo -e "$system: 未运行"
+        
         ;;
     2)
-        echo -e "sing-box: 未安装"
+        echo -e "$system: 未安装"
         ;;
     esac
-  
 }
+
+check_status() {
+    if [[ ! -f /etc/systemd/system/$system.service ]]; then
+        return 2
+    fi
+    temp=$(systemctl is-active $system)
+    if [[ $temp == "active" ]]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+check_install() {
+    check_status
+    if [[ $? == 2 ]]; then
+        echo ""
+        echo "请先安装$system"
+        if [[ $# == 0 ]]; then
+           wget https://raw.githubusercontent.com/52shell/sing-box-mosdns-fakeip/main/install-sing-box.sh && bash install-sing-box.sh
+        fi
+        return 1
+    else
+        return 0
+    fi
+}
+
+check_uninstall() {
+    check_status
+    if [[ $? != 2 ]]; then
+        echo ""
+        echo "$system已安装，请不要重复安装"
+        if [[ $# == 0 ]]; then
+            wget https://raw.githubusercontent.com/52shell/sing-box-mosdns-fakeip/main/install-sing-box.sh && bash install-sing-box.sh
+        fi
+        return 1
+    else
+        return 0
+    fi
+}
+
+install_singbox() {
+check_uninstall
+    echo -e "编译Sing-Box 最新版本"
+    sleep 1
+    apt -y install curl git build-essential libssl-dev libevent-dev zlib1g-dev gcc-mingw-w64
+    echo -e "开始编译Sing-Box 最新版本"
+    rm -rf /root/go/bin/*
+    Go_Version=$(curl https://github.com/golang/go/tags | grep '/releases/tag/go' | head -n 1 | gawk -F/ '{print $6}' | gawk -F\" '{print $1}')
+    # 判断 **CPU 架构
+    if [[ $(uname -m) == "aarch64" ]]; then
+        arch="arm64"
+    elif [[ $(uname -m) == "x86_64" ]]; then
+        arch="amd64"
+    else
+        arch="未知"
+        exit 0
+    fi
+    echo "系统架构是：$arch"
+    wget -O ${Go_Version}.linux-$arch.tar.gz https://go.dev/dl/${Go_Version}.linux-$arch.tar.gz
+    tar -C /usr/local -xzf ${Go_Version}.linux-$arch.tar.gz
+    echo 'export PATH=$PATH:/usr/local/go/bin' > /etc/profile.d/golang.sh
+    source /etc/profile.d/golang.sh
+    if ! go install -v -tags with_quic,with_grpc,with_dhcp,with_wireguard,with_ech,with_utls,with_reality_server,with_clash_api,with_gvisor,with_v2ray_api,with_lwip,with_acme github.com/sagernet/sing-box/cmd/sing-box@latest; then
+        echo -e "Sing-Box 编译失败！退出脚本"
+        exit 1
+    fi
+    echo -e "编译完成，开始安装"
+    sleep 1
+    # 检查是否存在旧版本的 sing-box
+    if [ -f "/usr/local/bin/sing-box" ]; then
+        echo "检测到已安装的 sing-box"
+        read -p "是否替换升级？(y/n): " replace_confirm
+        if [ "$replace_confirm" = "y" ]; then
+            echo "正在替换升级 sing-box"
+            cp "$(go env GOPATH)/bin/sing-box" /usr/local/bin/
+            echo "正在重启sing-box"
+            systemctl restart sing-box
+            echo "=================================================================="
+            echo -e "\t\t\tSing-Box 内核升级完毕"
+            echo -e "\t\t\tPowered by www.herozmy.com 2024"
+            echo "=================================================================="
+            exit 0  # 替换完成后停止脚本运行
+        else
+            echo "用户取消了替换升级操作"
+        fi
+    else
+        # 如果不存在旧版本，则直接安装新版本
+        wget https://raw.githubusercontent.com/52shell/sing-box-mosdns-fakeip/main/install-sing-box.sh && bash install-sing-box.sh
+    fi
+}
+
+install_singbox_p() {
+    echo -e "开始安装P_sing-box"
+    sleep 1
+    # 判断 CPU 架构
+    if [[ $(uname -m) == "aarch64" ]]; then
+        arch="armv8"
+    elif [[ $(uname -m) == "x86_64" ]]; then
+        arch="amd64"
+    else
+        arch="未知"
+        exit 0
+    fi
+    echo "系统架构是：$arch"
+
+    #拉取github每日凌晨自动编译的核心
+    wget -O sing-box-linux-$arch.tar.gz  https://raw.githubusercontent.com/52shell/herozmy-private/main/sing-box-puernya/sing-box-linux-$arch.tar.gz
+    sleep 1
+    echo -e "下载完成，开始安装"
+    sleep 1
+    tar -zxvf sing-box-linux-$arch.tar.gz
+    if [ -f "/usr/local/bin/sing-box" ]; then
+        echo "检测到已安装的 sing-box"
+        read -p "是否替换升级？(y/n): " replace_confirm
+        if [ "$replace_confirm" = "y" ]; then
+            echo "正在替换升级 sing-box"
+            mv sing-box /usr/local/bin/
+            systemctl restart sing-box
+            echo "=================================================================="
+            echo -e "\t\t\tSing-Box P核升级完毕"
+            echo -e "\t\t\tPowered by www.herozmy.com 2024"
+            echo "=================================================================="
+            exit 0  # 替换完成后停止脚本运行
+        else
+            echo "用户取消了替换升级操作"
+        fi
+    else
+        # 如果不存在旧版本，则直接安装新版本
+        wget https://raw.githubusercontent.com/52shell/sing-box-mosdns-fakeip/main/install-sing-box-p.sh && bash install-sing-box-p.sh
+    fi
+}
+
+uninstall_singbox() {
+    echo "开始卸载sing-box核心程序及其相关配置文件"
+    echo "卸载sing-box自启动"
+    systemctl disable sing-box
+    echo "关闭sing-box"
+    systemctl stop sing-box
+    echo "删除相关配置文件"
+    rm -rf /etc/sing-box
+    rm -f /usr/local/bin/sing-box
+    echo "卸载完成"
+}
+
+del_cache() {
+    echo "停止sing-box"
+    systemctl stop sing-box
+    rm -rf /etc/sing-box/cache.db
+    echo "sing-box缓存清理完成"
+    systemctl start sing-box
+}
+
+before_show_menu() {
+    echo && echo -n -e "按回车返回主菜单: " && read temp
+    checkcore
+}
+
 show_menu_singbox() {
     echo -e "
    面板管理脚本
   0. 退出脚本
 ————————————————
-  1. 安装 sing-box
-  2. 更新 sing-box核心
-  3. 更新 Puer-sing-box核心
-  4. 卸载 sing-box
-  5. 清理 sing-box缓存
+  1. 安装/更新 sing-box
+  2. 安装/更新 Puer-sing-box核心
+  3. 卸载 sing-box
+  4. 清理 sing-box缓存
+  5. 启动/停止 sing-box 
  "
-    show_status
-    echo && read -p "请输入选择 [0-16]: " num
+show_status
+    echo && read -p "请输入选择 [0-5]: " num
+		
+    case "${num}" in
+    0)
+        exit 0
+        ;;
+    1)
+        check_install && install_singbox
+        ;;
+    2)
+        check_install && install_singbox_p
+        ;;
+    3)
+        check_install && uninstall_singbox
+        ;;
+    4)
+        check_install && del_cache
+        ;;
+    5)
+        check_install && toggle_service
+        ;;
+    *)
+        echo "请输入正确的数字 [0-5]"
+        ;;
+    esac
+}
+
+show_menu_mosdns() {
+    echo -e "
+   面板管理脚本
+  0. 退出脚本
+————————————————
+  1. 安装mosdns
+  4. 卸载 mosdns
+  5. 清理 mosdns 缓存
+  6. 启动 sing-box 
+ "
+    echo && read -p "请输入选择 [0-6]: " num
 
     case "${num}" in
     0)
         exit 0
         ;;
     1)
-        check_uninstall && install
+        check_uninstall && install_mosdns
         ;;
     2)
-        check_install && update
+        check_install && update_mosdns
         ;;
     3)
-        check_install && uninstall
+        check_install && uninstall_mosdns
         ;;
     4)
-        check_install && reset_user
+        check_install && reset_mosdns_cache
         ;;
     5)
-        check_install && reset_config
-        ;;
-    6)
-        check_install && set_port
-        ;;
-    7)
-        check_install && check_config
-        ;;
-    8)
-        check_install && start
-        ;;
-    9)
-        check_install && stop
+        check_install && toggle_service_mosdns
         ;;
     *)
-        echo "请输入正确的数字 [0-16]"
+        echo "请输入正确的数字 [0-6]"
         ;;
     esac
 }
 
-check_sing-box_status() {
-    if [[ ! -f /etc/systemd/system/sing-box.service ]]; then
-        return 2
-    fi
-    temp=$(systemctl status sing-box | grep Active | awk '{print $3}' | cut -d "(" -f2 | cut -d ")" -f1)
-    if [[ x"${temp}" == x"running" ]]; then
-        return 0
+toggle_service() {
+    if systemctl is-active --quiet sing-box; then
+        systemctl stop sing-box
+        echo "sing-box 已停止"
     else
-        return 1
+        systemctl start sing-box
+        echo "sing-box 已启动"
     fi
 }
 
-check_mosdns_status() {
-    if [[ ! -f /etc/systemd/system/mosdns.service ]]; then
-        return 2
-    fi
-    temp=$(systemctl status mosdns | grep Active | awk '{print $3}' | cut -d "(" -f2 | cut -d ")" -f1)
-    if [[ x"${temp}" == x"running" ]]; then
-        return 0
+install_mosdns() {
+    echo "安装mosdns的代码未提供"
+}
+
+update_mosdns() {
+    echo "更新mosdns的代码未提供"
+}
+
+uninstall_mosdns() {
+    echo "卸载mosdns的代码未提供"
+}
+
+reset_mosdns_cache() {
+    echo "重置mosdns缓存的代码未提供"
+}
+
+toggle_service_mosdns() {
+    if systemctl is-active --quiet mosdns; then
+        systemctl stop mosdns
+        echo "mosdns 已停止"
     else
-        return 1
+        systemctl start mosdns
+        echo "mosdns 已启动"
     fi
+}
+
+system=""
+
+# 循环直到用户输入正确的值或选择退出
+while true; do
+    prompt_user
+    read -p "请输入选择 [0-2]: " choice
+
+    if [[ $choice -eq 1 ]]; then
+        system="sing-box"
+        break
+    elif [[ $choice -eq 2 ]]; then
+        system="mosdns"
+        break
+    elif [[ $choice -eq 0 ]]; then
+        echo "退出脚本"
+        exit 0
+    else
+        echo "无效输入，请输入1、2或0。"
+    fi
+done
+echo "进入 $system 管理菜单"
+sleep 1
+
+checkcore
